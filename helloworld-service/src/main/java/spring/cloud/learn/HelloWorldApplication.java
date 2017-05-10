56970bf834e84a4d523625d6e905f2c9bb126880
wushibin <963919269@qq.com>
2017-05-10 22:38:45 +0800

    Add helloworld sample files.

---
+++ helloworld-service/src/main/java/spring/cloud/learn/HelloWorldApplication.java
@@ -0,0 +1,35 @@
+package spring.cloud.learn;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@EnableEurekaClient
+@SpringBootApplication
+public class HelloWorldApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(HelloWorldApplication.class, args);
+    }
+}
+
+@RestController
+class ServiceInstanceRestController {
+
+    @Autowired
+    private DiscoveryClient discoveryClient;
+
+    @RequestMapping("/service-instances/{applicationName}")
+    public List<ServiceInstance> serviceInstancesByApplicationName(
+            @PathVariable String applicationName) {
+        return this.discoveryClient.getInstances(applicationName);
+    }
+}
+
---
+++ helloworld-service/src/test/java/spring/cloud/learn/HelloWorldApplicationTest.java
@@ -0,0 +1,63 @@
+package spring.cloud.learn;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.context.embedded.LocalServerPort;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = HelloWorldApplication.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class HelloWorldApplicationTest {
+    @Configuration
+    @EnableAutoConfiguration
+    @EnableEurekaServer
+    static class EurekaServer {
+    }
+
+    static ConfigurableApplicationContext eurekaServer;
+
+    @BeforeClass
+    public static void startEureka() {
+        eurekaServer = SpringApplication.run(EurekaServer.class,
+                "--server.port=8762",
+                "--eureka.instance.leaseRenewalIntervalInSeconds=1");
+    }
+
+    @AfterClass
+    public static void closeEureka() {
+        eurekaServer.close();
+    }
+
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private TestRestTemplate testRestTemplate;
+
+    @Test
+    public void shouldRegisterClientInEurekaServer() throws InterruptedException {
+        // registration has to take place...
+        Thread.sleep(5000);
+
+        ResponseEntity<String> response = this.testRestTemplate.getForEntity("http://localhost:" + this.port + "/service-instances/helloword-service", String.class);
+
+        assertEquals(response.getStatusCode(), HttpStatus.OK);
+        assertTrue(response.getBody().contains("helloword-service"));
+    }
+
+}
